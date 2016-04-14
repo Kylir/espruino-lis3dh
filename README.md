ce7aee963b4d285a20b8c930e95e37b640367e72
Kylir <Kylir@users.noreply.github.com>
2016-04-14 17:04:08 +0100

    Refactor the read function. Meaningfull readings now

--- README.md
+++ README.md
@@ -1,19 +1,49 @@
 # espruino-lis3dh
 
-The goal of this project is to play with an Adafruit LIS3DH accelerometer plugged to an ESP8266 breakout board. The ESP is an Adafruit Huzzah flashed with Espruino.
-All the communication are using the I2C protocol.
+The goal of this project is ultimately to provide an Espruino module for the LIS3DH.
 
-I'm following the [tutorial from Adafruit.](https://learn.adafruit.com/adafruit-lis3dh-triple-axis-accelerometer-breakout)
-
-
-## Wiring
-
-TO DO
+For the moment this is work in progress and it is far from completion! Right now I'm still playing with an Adafruit LIS3DH accelerometer plugged to an ESP8266 breakout board. The ESP is an Adafruit Huzzah flashed with Espruino.
+All the communication are using the I2C protocol and I'm following the [tutorial from Adafruit.](https://learn.adafruit.com/adafruit-lis3dh-triple-axis-accelerometer-breakout)
 
 ## LIS3DH Datasheet
 
-The datasheet for the LIS3DH is available [here](http://www.adafruit.com/datasheets/LIS3DH.pdf).
+The datasheet for the LIS3DH is available [here](http://www.st.com/web/en/resource/technical/document/datasheet/CD00274221.pdf).
 
 ## Example
 
-TO DO
+### Set-up I2C
+
+SDA pin is on pin 4 and SCL is on pin 5. Edit the end of `index.js` if you are using another wiring.
+
+The following code is executed at the end of index.js:
+
+```js
+I2C1.setup({sda: D4, scl: D5});
+```
+
+### Instantiate the object
+
+Then, we create the main object and set all the default configurations.
+The default I2C address is `0x18`.
+
+```js
+var accel = new LIS3DH(I2C1, 0x18);
+accel.begin();
+```
+
+
+### Display the acceleration values
+
+You can then trigger the read function by calling the `read()` function:
+
+```js
+console.log(accel.read());
+```
+
+You should see something like this:
+
+```js
+[ 16880, 2752, 2528 ]
+=undefined
+```
+
--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function LIS3DH (TheI2c, TheAddr) {
     };
 
     this.setDataRate = function (dataRate) {
-        var ctl1 = this.readRegister8(LIS3DH_REG.CTRL1);        
+        var ctl1 = this.readRegister8(LIS3DH_REG.CTRL1);
         ctl1 &= ~(0xF0); // mask off bits
-        ctl1 |= (dataRate << 4);        
+        ctl1 |= (dataRate << 4);
         this.writeRegister8(LIS3DH_REG.CTRL1, ctl1);
     };
 
@@ -32,16 +32,19 @@ function LIS3DH (TheI2c, TheAddr) {
 
     // Set the chip modes: 
     this.begin = function () {
-        // enable all axes, normal mode
-        this.writeRegister8(LIS3DH_REG.CTRL1, 0x07); //0000 0111
-        // 400Hz rate
-        this.setDataRate(LIS3DH_DATARATE['400_HZ']);
-        // High res & BDU enabled
+        // enable all axes, normal mode and 400Hz refresh rate
+        this.writeRegister8(LIS3DH_REG.CTRL1, 0x77); //0000 0111
+        // CTRL2 - Everything at 0
+        //this.writeRegister8(LIS3DH_REG.CTRL2, 0x00);
+        
+        // No interrupt (is set to 0x10 in Adafruit tuto...)
+        //this.writeRegister8(LIS3DH_REG.CTRL3, 0x00);
+        
+        // Block Data Update: continuous, LSB at lower address, 2G, High res
         this.writeRegister8(LIS3DH_REG.CTRL4, 0x88); //1000 1000
-        // DRDY on INT1
-        this.writeRegister8(LIS3DH_REG.CTRL3, 0x00); //Was 0x10 - 0001 0000
-        // enable adcs
-        //this.writeRegister8(LIS3DH_REG.TEMPCFG, 0x80);
+        
+        // enable adcs? not now.
+        //this.writeRegister8(LIS3DH_REG.TEMPCFG, 0x00);
     };
 
     this.read = function () {
@@ -50,62 +53,64 @@ function LIS3DH (TheI2c, TheAddr) {
 
         this.i2c.writeTo(this.addr, LIS3DH_REG.OUT_X_L | 0x80 ); //1000 0000 | 
         readings = this.i2c.readFrom(this.addr, 6);
-        console.log('0: ' + readings[0]);        
-        console.log('1: ' + readings[1]);        
-        console.log('2: ' + readings[2]);        
-        console.log('3: ' + readings[3]);        
-        console.log('4: ' + readings[4]);        
-        console.log('5: ' + readings[5]);       
         
-        /*
-        var x = readings[0] | (readings[1] << 8);
-        var y = readings[2] | (readings[3] << 8);
-        var z = readings[4] | (readings[5] << 8);
-        */
+        var x = (((readings[1] & 0xff) << 8) | (readings[0] & 0xff));
+        if (x >= 0x8000) {
+            x = -(0x10000 - x);
+        }
+        var y = (((readings[3] & 0xff) << 8) | (readings[2] & 0xff));
+        if (y >= 0x8000) {
+            y = -(0x10000 - y);
+        }
+        var z = (((readings[5] & 0xff) << 8) | (readings[4] & 0xff));
+        if (z >= 0x8000) {
+            z = -(0x10000 - z);
+        }
 
-        //console.log('x=' + x + ', y='+ y + ', z=' + z);
+        return [x, y, z];   
+        
     };
 
     var LIS3DH_DEFAULT_ADDRESS = 0x18;
 
     // Registers
     var LIS3DH_REG = {
-        STATUS1: 0x07,
-        OUTADC1_L: 0x08,
-        OUTADC1_H: 0x09,
-        OUTADC2_L: 0x0A,
-        OUTADC2_H: 0x0B,
-        OUTADC3_L: 0x0C,
-        OUTADC3_H: 0x0D,
-        INTCOUNT: 0x0E,
-        WHOAMI: 0x0F,
-        TEMPCFG: 0x1F,
-        CTRL1: 0x20,
-        CTRL2: 0x21,
-        CTRL3: 0x22,
-        CTRL4: 0x23,
-        CTRL5: 0x24,
-        CTRL6: 0x25,
-        REFERENCE: 0x26,
-        STATUS2: 0x27,
-        OUT_X_L: 0x28,
-        OUT_X_H: 0x29,
-        OUT_Y_L: 0x2A,
-        OUT_Y_H: 0x2B,
-        OUT_Z_L: 0x2C,
-        OUT_Z_H: 0x2D,
-        FIFOCTRL: 0x2E,
-        FIFOSRC: 0x2F,
-        INT1CFG: 0x30,
-        INT1SRC: 0x31,
-        INT1THS: 0x32,
-        INT1DUR: 0x33,
-        CLICKCFG: 0x38,
-        CLICKSRC: 0x39,
-        CLICKTHS: 0x3A,
-        TIMELIMIT: 0x3B,
-        TIMELATENCY: 0x3C,
-        TIMEWINDOW: 0x3D
+        'STATUS1': 0x07,
+        'OUTADC1_L': 0x08,
+        'OUTADC1_H': 0x09,
+        'OUTADC2_L': 0x0A,
+        'OUTADC2_H': 0x0B,
+        'OUTADC3_L': 0x0C,
+        'OUTADC3_H': 0x0D,
+        'INTCOUNT': 0x0E,
+        'WHOAMI': 0x0F,
+        'TEMPCFG': 0x1F,
+        'CTRL1': 0x20,
+        'CTRL2': 0x21,
+        'CTRL3': 0x22,
+        'CTRL4': 0x23,
+        'CTRL5': 0x24,
+        'CTRL6': 0x25,
+        'REFERENCE': 0x26,
+        'STATUS2': 0x27,
+        'OUT_X_L': 0x28,
+        'OUT_X_H': 0x29,
+        'OUT_Y_L': 0x2A,
+        'OUT_Y_H': 0x2B,
+        'OUT_Z_L': 0x2C,
+        'OUT_Z_H': 0x2D,
+        'FIFOCTRL': 0x2E,
+        'FIFOSRC': 0x2F,
+        'INT1CFG': 0x30,
+        'INT1SRC': 0x31,
+        'INT1THS': 0x32,
+        'INT1DUR': 0x33,
+        'CLICKCFG': 0x38,
+        'CLICKSRC': 0x39,
+        'CLICKTHS': 0x3A,
+        'TIMELIMIT': 0x3B,
+        'TIMELATENCY': 0x3C,
+        'TIMEWINDOW': 0x3D
     };
 
     // Ranges
@@ -118,9 +123,9 @@ function LIS3DH (TheI2c, TheAddr) {
 
     // Axis
     var LIS3DH_AXIS = {
-        X: 0x0,
-        Y: 0x1,
-        Z: 0x2
+        'X': 0x0,
+        'Y': 0x1,
+        'Z': 0x2
     };
 
     // Data rate
@@ -142,13 +147,10 @@ function LIS3DH (TheI2c, TheAddr) {
 I2C1.setup({sda: D4, scl: D5});
 
 var accel = new LIS3DH(I2C1, 0x18);
+accel.begin();
 
 // Accelerometer ID
 console.log('Chip ID: ' + accel.whoami()); // Should display 51.
 
-accel.begin();
-
 accel.read();
-
-
 
